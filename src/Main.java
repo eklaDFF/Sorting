3975074a2133bdbc31c53b2bee6cc5f1a4194a51
eklaDFF <rahulkrsingh160086@gmail.com>
2024-01-14 15:00:46 +0530

    Selection Sort Algorithm Added

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,16 @@ public class Main {
         for (int n : arr){
             System.out.print(n + ",");
         }
+
+
+
+        System.out.println("\n-------------------Example for Selection Sort-----------------");
+        int[] arrSelection = {45,67,33,42,90,23,-9};
+        SelectionSort(arrSelection);
+
+        for (int n : arrSelection){
+            System.out.print(n + ",");
+        }
     }
     public static void BubbleSort(int[] arr){
         for(int i= arr.length-2;i>=0;i--){
@@ -18,6 +28,21 @@ public class Main {
                     arr[j+1] = t;
                 }
             }
+        }
+    }
+
+    public static void SelectionSort(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            int indexOfMinimumNumber = i;
+            for (int j = i; j< arr.length; j++){
+                if (arr[j]<arr[indexOfMinimumNumber]){
+                    indexOfMinimumNumber = j;
+                }
+            }
+
+            int minimumNumber = arr[indexOfMinimumNumber];
+            arr[indexOfMinimumNumber] = arr[i];
+            arr[i] = minimumNumber;
         }
     }
 }
